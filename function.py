0304ca846e1057a707a20ac0bbbf04e09a721718
Denver Bradley <denverbradley@Denvers-MacBook-Air.local>
2022-09-05 12:34:30 -0400

    modified interface so it will run until user selects 9

--- function.py
+++ function.py
@@ -2,8 +2,10 @@ def interface():
     print("Blood Calculator")
     print("Options:")
     print("9 - Quit")
-    choice = input("Enter your choice: ")
-    if choice=='9':
-        return
+    keep_running = True
+    while keep_running:
+        choice = input("Enter your choice: ")
+        if choice=='9':
+            return
    
 interface()
