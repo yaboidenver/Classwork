6dbe04a391d618b253882bf6444de92617aaf2a8
Denver Bradley <denverbradley@Denvers-MacBook-Air.local>
2022-09-05 12:32:06 -0400

    created blood calculator function

---
+++ function.py
@@ -0,0 +1,9 @@
+def interface():
+    print("Blood Calculator")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice=='9':
+        return
+   
+interface()
