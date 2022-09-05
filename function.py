e59957b2ffc8031844d4496716815f5d373dc71f
Denver Bradley <denverbradley@Denvers-MacBook-Air.local>
2022-09-05 12:53:11 -0400

    added HDL check for its characterization

--- function.py
+++ function.py
@@ -8,8 +8,15 @@ def interface():
         if choice=='9':
             return
 
-def check_HDL():
+def input_HDL():
     HDL_input = input("Enter the HDL value:")
     return int(HDL_input)
-    
+
+def check_HDL(HDL_input):
+    if HDL_input >= 60:
+        return "Normal"
+    elif HDL_input < 60 & HDL_input >= 40:
+        return "Borderline low"
+    else:
+        return "Low"
 interface()
