72d7c51f1774c40aff816a075a170eef67f4d512
Denver Bradley <denverbradley@Denvers-MacBook-Air.local>
2022-09-05 12:58:12 -0400

    added driver function to output answer of calculator

--- function.py
+++ function.py
@@ -15,8 +15,17 @@ def input_HDL():
 def check_HDL(HDL_input):
     if HDL_input >= 60:
         return "Normal"
-    elif HDL_input < 60 & HDL_input >= 40:
+    elif HDL_input >= 40:
         return "Borderline low"
     else:
         return "Low"
+
+def HDL_Driver():
+    hdl_value = input_HDL()
+    answer = check_HDL(hdl_value)
+    output_HDL_result(hdl_value, answer)
+
+def output_HDL_result(hdl_value,charac):
+    print("The results for an HDL value of {} is {}".format(hdl_value, charac))
+
 interface()
