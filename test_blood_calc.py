cf1af1e6ca4c85383947e2198613446762dcdc65
Denver Bradley <denverbradley@Denvers-Air.lan>
2022-09-19 19:17:05 -0400

    added test for cholesterol check

--- test_blood_calc.py
+++ test_blood_calc.py
@@ -9,6 +9,7 @@ def test_check_HDL_norm(HDL_input, expected):
     answer = check_HDL(HDL_input)
     assert answer == expected
 
+
 @pytest.mark.parametrize('LDL_input, expected',
     [(195, "Very High"),
     (165, "High"),
@@ -20,6 +21,14 @@ def test_check_LDL(LDL_input, expected):
     assert answer == expected
 
 
+@pytest.mark.parametrize('chol_input, expected',
+    [(245, "High"),
+    (235, "Borderline High"),
+    (180, "Normal")])
+def test_check_Chol(chol_input, expected):
+    from blood_calc import check_Chol
+    answer = check_Chol(chol_input)
+    assert answer == expected
 
 """
 def test_check_HDL_borderline():
